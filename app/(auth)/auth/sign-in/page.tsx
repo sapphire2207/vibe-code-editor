fbcd2f2f62ad9f5fb13551a7eb0c3bc94ea5f800
Sapphire <sreem7808@gmail.com>
2025-10-03 20:28:03 +0530

    03: Authentication

---
+++ app/(auth)/auth/sign-in/page.tsx
@@ -0,0 +1,14 @@
+import SignInFormClient from '@/features/auth/components/signin-form-client'
+import Image from 'next/image'
+import React from 'react'
+
+const SignInPage = () => {
+  return (
+    <div className='space-y-6 flex flex-col items-center justify-center'>
+        <Image src={"/logo.svg"} alt='Logo Image' height={300} width={300} />
+        <SignInFormClient />
+    </div>
+  )
+}
+
+export default SignInPage
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button";
+import UserButton from "@/features/auth/components/user-button";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold text-rose-500">Vibe Code Editor</h1>
-      <Button>Hare Krishna!</Button>
+      <UserButton />
     </div>
   );
 }
