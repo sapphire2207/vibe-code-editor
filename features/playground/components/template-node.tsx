c648d1513b3ed5262e515c95efc7b8f98bbb771f
Sapphire <sreem7808@gmail.com>
2025-10-10 14:31:59 +0530

    09 Playground Editor Added

--- features/playground/components/template-node.tsx
+++ features/playground/components/template-node.tsx
@@ -151,7 +151,7 @@ const TemplateNode = ({
         return (
             <SidebarMenuItem>
                 <div className="flex items-center group">
-                    <SidebarMenuButton className="flex-1">
+                    <SidebarMenuButton isActive={isSelected} onClick={()=> onFileSelect?.(file)} className="flex-1">
                         <File className="h-4 mr-2 w-4 shrink-0" />
                         <span>{fileName}</span>
                     </SidebarMenuButton>
